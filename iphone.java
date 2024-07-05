9b4413b35280292df94b619768a6dba94c7fddc9
Michel-Santana <michelsantanajobs@gmail.com>
2024-07-05 12:13:19 -0300

    FinalProjetoIphoneDIO

---
+++ iphone.java
@@ -0,0 +1,52 @@
+public class iphone implements navegadorInternet,aparelhoTelefonico,reprodutorMusical {
+
+    @Override
+    public void adicionarNovaAba() {
+        
+        
+    }
+
+    @Override
+    public void atualizarPagina() {
+        
+        
+    }
+
+    @Override
+    public void exibirPagina() {
+        
+        
+    }
+
+    @Override
+    public void atender() {
+        
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        
+    }
+
+    @Override
+    public void ligar() {
+        
+    }
+
+    @Override
+    public void pausar() {
+        
+    }
+
+    @Override
+    public void selecionarMusica() {
+        
+    }
+
+    @Override
+    public void tocar() {
+        
+    }
+   
+    
+}
